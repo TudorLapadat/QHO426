44b30ef6723d0fa9c26e154d6c974a955c8037b3
TudorLapadat <>
2022-06-30 18:58:44 +0000

    Added data types

---
+++ basics/input/data_types.py
@@ -0,0 +1,11 @@
+print("What is your name human?")
+name = input()
+print("How old are you (in years)?")
+age = int(input())
+print("How tall are you (in meters)?")
+height = float(input())
+print("How much do you weigh (in kilograms)?")
+weight = float(input())
+bmi = weight/height**2
+print(f"{name} you are {age} years old and your bmi is {bmi:.2f}.",)
+
